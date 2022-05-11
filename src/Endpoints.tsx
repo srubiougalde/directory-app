2c0529ef68d90bca9d7e97ee73f2f26c0ae58754
Sergio Rubio <srubio@heicommmunity.com>
2022-05-11 11:40:15 -0500

    Display list of members

--- src/Endpoints.tsx
+++ src/Endpoints.tsx
@@ -5,3 +5,6 @@ export const RESET = "https://localhost:5001/api/Accounts/authcode"
 export const FORGOT = "https://localhost:5001/api/Accounts/forgotPassword"
 export const RESETPASSWORD = "https://localhost:5001/api/Accounts/me/resetPassword"
 export const CHANGEMYPASSWORD = "https://localhost:5001/api/Accounts/me/changePassword"
+// Directory endpoints
+export const MEMBERS = "https://localhost:5001/api/Directory/members";
+export const FRIENDSHIP = (memberId: string) =>  `https://localhost:5001/api/Directory/members/${memberId}/friends`;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,26 @@
 import React from "react";
+import axios from "axios";
 import withLayout from "../hoc/withLayout.hoc";
 import PageTitle from "../components/page-title/PageTitle.component";
 import { PageBox } from "../components/page-box/PageBox.styled";
 import { 
   DataGrid,
+  GridColDef,
+  GridToolbarContainer,
+  GridToolbarFilterButton,
+  GridToolbarExport,
   enUS,
+  GridCellParams,
+  GridOverlay,
 } from "@material-ui/data-grid";
+import LinearProgress from "@material-ui/core/LinearProgress";
 import { useAppState } from "../providers/State.provider";
 import {
   StyledPageHeader,
   StyledPageSubHeader,
 } from "./Styled/Home.Styled";
 import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import { MEMBERS } from "../Endpoints";
 import { PRIMARYCOLOR  } from "../Utils";
 
 const theme = createTheme(
@@ -23,8 +32,79 @@ const theme = createTheme(
   enUS
 );
 
+function CustomToolbar() {
+  return (
+    <GridToolbarContainer>
+      <GridToolbarFilterButton />
+      <GridToolbarExport />
+    </GridToolbarContainer>
+  );
+}
+  
+function CustomLoadingOverlay() {
+  return (
+    <GridOverlay>
+      <div style={{ position: "absolute", top: 0, width: "100%" }}>
+        <LinearProgress />
+      </div>
+    </GridOverlay>
+  );
+}
+  
+const defaultColumns: GridColDef[] = [
+  {
+    field: "name",
+    headerName: "Name",
+    flex: 3,
+  },
+  {
+    field: "websiteShortUrl",
+    headerName: "Website",
+    flex: 2,
+    renderCell: (params: GridCellParams) => (
+      <a href={params.row.websiteUrl} target='_blank'>{params.row.websiteShortUrl}</a>
+    )
+  },
+  { 
+    field: "friendsCount",
+    headerName: "Friends",
+    flex: 1
+  }
+];
+
+type Members = {
+  id: string;
+  name: string;
+  websiteUrl: string;
+  websiteShortUrl: string;
+  friendsCount: string;
+};
+
 const Home: React.FC = () => {
   const { state } = useAppState();
+  const [members, setMembers] = React.useState<Members[]>([]);
+  const [loading, setLoading] = React.useState(false);
+  const [loadingMembers, setLoadingMembers] = React.useState(false);
+
+  const columns = [
+    ...defaultColumns,
+  ];
+
+  const loadMembers = async () => {
+    setLoadingMembers(true);
+    try {
+      const { data } = await axios.get(`${MEMBERS}`);
+      setMembers(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoadingMembers(false);
+    }
+  };
+
+  React.useEffect(() => {
+    loadMembers();
+  }, [state]);
 
   return (
     <div>
@@ -38,7 +118,16 @@ const Home: React.FC = () => {
           <div style={{ display: "flex", height: "100%" }}>
             <div style={{ flexGrow: 1 }}>
               <ThemeProvider theme={theme}>
-                
+                <DataGrid
+                  disableSelectionOnClick
+                  columns={columns}
+                  rows={members}
+                  loading={loading}
+                  components={{
+                    Toolbar: CustomToolbar,
+                    LoadingOverlay: CustomLoadingOverlay,
+                  }}
+                />
               </ThemeProvider>
             </div>
           </div>
