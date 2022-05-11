f0de33bc287b7e7ac7b9fb34b655241d2137c806
Sergio Rubio <srubio@heicommmunity.com>
2022-05-11 11:30:26 -0500

    Fix missing AppStateProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import "./App.css";
 import moment from "moment";
 
+import AppStateProvider from "./providers/State.provider";
 import Main from "./pages/Main";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import MomentUtils from "@date-io/moment";
 import "moment/locale/es";
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 
 moment.locale("es"); // it is required to select default locale manually
 
 function App() {
   return (
-    <div>
+    <AppStateProvider>
         <MuiPickersUtilsProvider utils={MomentUtils} locale={'en'}>
           <Main />
         </MuiPickersUtilsProvider>
         <ToastContainer />
-    </div>
+    </AppStateProvider>
   );
 }
 
